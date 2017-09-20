c094a36a0e642dd56173154e36eaf42f210fb05a
msano <kz56cd@gmail.com>
2017-09-20 17:42:43 +0900

    Add storing temperature data logic

--- preHealthKit/ViewController.swift
+++ preHealthKit/ViewController.swift
@@ -7,19 +7,92 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+    // MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    // MARK: action
+    @IBAction func AddTemperatureButtonTapped(_ sender: UIButton) {
+        didTapAddData()
+    }
 
+}
+
+extension ViewController {
+    
+    func didTapAddData() {
+        saveHealthData()
+    }
+    
+    // MARK: private
+    private func saveHealthData() {
+        guard let temperature = Double("36.0"),
+            let type = HKObjectType.quantityType(forIdentifier: .bodyTemperature) else {
+            return
+        }
+        
+        // 単位
+        let unit = HKUnit.degreeCelsius()
+        
+        // データ保存領域の生成
+        let healthStore = HKHealthStore()
+        
+        let healthData = HKQuantity(
+            unit: unit,
+            doubleValue: temperature
+        )
+        
+        let sample = HKQuantitySample(
+            type: type,
+            quantity: healthData,
+            start: Date(),
+            end: Date()
+        )
+        
+        let authorizedStatus = healthStore.authorizationStatus(for: type)
+        
+        if authorizedStatus == .sharingAuthorized {
+            healthStore.save(sample, withCompletion: { (success, error) in
+                if error != nil {
+                    print(error.debugDescription)
+                    return
+                }
+                
+                if success {
+                    healthStore.save(sample, withCompletion:{ (success, error) in
+                        print("保存成功")
+                    })
+                }
+            })
+        } else {
+            
+            healthStore.requestAuthorization(
+                toShare: [type],
+                read: [type],
+                completion: { (success, error) in
+            
+                if error != nil {
+                    print(error.debugDescription)
+                    return
+                }
+                
+                // アクセス権限を得たらデータを保存
+                if success {
+                    healthStore.save(sample, withCompletion:{ (success, error) in
+                        print("保存成功")
+                    })
+                }
+            })
+        }
+    }
 }
 
